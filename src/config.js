7e582c7c7fb6da0062c56a52e92cd02ac306360b
Alexander <alexander.y.peng@gmail.com>
2020-02-15 19:06:13 -0800

    Adds netlify build script and updates config

--- src/config.js
+++ src/config.js
@@ -1,18 +1,43 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: 'us-west-1',
-    BUCKET: 'serverless-notes-app-upload-tutorial'
+    REGION: 'us-east-2',
+    BUCKET: 'notes-app-2-api-dev-attachmentsbucket-12gh851ieq4qw'
   },
   apiGateway: {
     REGION: 'us-east-2',
-    URL: 'https://bc62l1fot5.execute-api.us-east-2.amazonaws.com/prod'
+    URL: 'https://ddfj6f1ugf.execute-api.us-east-2.amazonaws.com/dev'
   },
   cognito: {
     REGION: 'us-east-2',
-    USER_POOL_ID: 'us-east-2_CEbE9BJP9',
-    APP_CLIENT_ID: '3hdv46hd16hsv8ph03mlgp4n0r',
-    IDENTITY_POOL_ID: 'us-east-2:9185e791-01a3-49e5-a7d4-9f3c0eda1b99'
+    USER_POOL_ID: 'us-east-2_e1IxKqLbD',
+    APP_CLIENT_ID: '18j3uioqpqvfevkdthh5j5h9t1',
+    IDENTITY_POOL_ID: 'us-east-2:20519950-5193-4dc1-95d0-20d0bc8acdff'
   }
+}
+
+const prod = {
+  s3: {
+    REGION: 'us-east-2',
+    BUCKET: 'notes-app-2-api-prod-attachmentsbucket-s3v4jkg66x7z'
+  },
+  apiGateway: {
+    REGION: 'us-east-2',
+    URL: 'https://krtdwp132f.execute-api.us-east-2.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-2',
+    USER_POOL_ID: 'us-east-2_Maa9nV2xZ',
+    APP_CLIENT_ID: '6ac9chtmg2gim6hsu0pv4cv9f2',
+    IDENTITY_POOL_ID: 'us-east-2:d93ff323-8700-42c2-8027-c1811863d942'
+  }
+}
+
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 }
 
